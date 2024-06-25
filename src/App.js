132b5255c5c84dac6956d7cba76e8c8ae0e16902
lorstenoplo <nishanthdipali@gmail.com>
2024-06-25 13:46:27 +0530

    fixed login, signup

--- src/App.js
+++ src/App.js
@@ -1,53 +1,83 @@
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Alert from './component/Alert';
-import { useState } from 'react';
-// import Login from './pages/Login';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import ProtectedRoute from "./component/ProtectedRoute";
+import Alert from "./component/Alert";
 
-
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
-
-  const [Mode,setMode] = useState("Dark Mode");//Wther dark mode is enabled or not
-  const toggleMode=()=>{
-    if (Mode === 'Dark Mode'){
+  const [Mode, setMode] = useState("Dark Mode"); //Wther dark mode is enabled or not
+  const toggleMode = () => {
+    if (Mode === "Dark Mode") {
       setMode("Light Mode");
-      showAlert("Dark Mode is enabled","good");
-      document.getElementById('mode').className = "dark h-full";
-    }
-    else{
+      showAlert("Dark Mode is enabled", "good");
+      document.getElementById("mode").className = "dark h-full";
+    } else {
       setMode("Dark Mode");
-      showAlert("Light Mode is enabled","good");
-      document.getElementById('mode').className = "light h-full";
+      showAlert("Light Mode is enabled", "good");
+      document.getElementById("mode").className = "light h-full";
     }
-  }
+  };
   const [alert, setAlert] = useState(null);
-  const showAlert=(message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
+      msg: message,
+      type: type,
     });
-    setTimeout(()=>{
-      setAlert(null)
-    },2000);
-  }
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
   return (
-    <div  id="mode" className='light h-full'>
-    <Router> 
-    <Alert alert={alert}/>
-    <Routes>
-    <Route exact path="/" element={<Signup showAlert={showAlert} mode={Mode} toggleMode={toggleMode}/>}/>
-    <Route exact path="/login" element={<Login showAlert={showAlert} mode={Mode} toggleMode={toggleMode}/>}/>
-  </Routes>  
-   </Router> 
-   </div>
+    <div id="mode" className="light h-full">
+      <Router>
+        <Alert alert={alert} />
+        <Routes>
+          <Route
+            exact
+            path="/signup"
+            element={
+              <Signup
+                showAlert={showAlert}
+                mode={Mode}
+                toggleMode={toggleMode}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/login"
+            element={
+              <Login
+                showAlert={showAlert}
+                mode={Mode}
+                toggleMode={toggleMode}
+              />
+            }
+          />
+
+          {/* Protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route
+              exact
+              path="/"
+              element={
+                <Home
+                  showAlert={showAlert}
+                  mode={Mode}
+                  toggleMode={toggleMode}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/component/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
+const ProtectedRoute = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    }
+  }, [location.pathname]);
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
